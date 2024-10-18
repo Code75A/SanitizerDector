a446a251861867e779a3681d4b0abcb6ed34db32
ITWOI <fff000147369@gmail.com>
2024-10-18 15:26:34 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 CC=/bigdata/fff000/UBGen/llvm_under_tested/LLVM-19.1.0-Linux-X64/bin/clang-19
 MutantHome=/bigdata/fff000/UBGen/mutants
@@ -14,8 +14,19 @@ genCompileCommand() {
 
 runCommandCheckString() {
     strtoCheck=$1
+    programName=$2
 
-    error_message=$(./a.out 2>&1)
+    while true
+    do
+        error_message=$(timeout 10 ./a.out 2>&1)
+        exit_status=$?
+        if [ $exit_status -eq 124 ]; then
+            echo "./a.out command timeout for $programName"
+            echo "./a.out command timeout for $programName" >> $OUTPUTFILE
+        else
+            break
+        fi
+    done
     # Check if the error message contains a specific string
     if echo "$error_message" | grep -q $strtoCheck; then
         return 1
@@ -31,17 +42,18 @@ testOneProgram() {
     $CC -g -O0 -Wno-everything -I$CSMITH_PATH -fsanitize=undefined $programName &> /dev/null
     sleep 0.5
 
-    runCommandCheckString "UndefinedBehaviorSanitizer"
+    echo "runing UndefinedBehaviorSanitizer"
+    runCommandCheckString "UndefinedBehaviorSanitizer" $programName
     res1=$?
     echo "UBS finished"
 
     #run sseq
     genCompileCommand $programName > $MutantHome/compile_commands.json
-    res=`timeout 10 $SSEQ $programName 2>1`
+    res=`timeout 20 $SSEQ $programName 2>1`
     exit_status=$?
     # if sseq failed, we skip this file
     if [ $exit_status -eq 124 ]; then
-        echo "sseq command timeout" >> $OUTPUTFILE
+        echo "sseq command timeout for $programName" >> $OUTPUTFILE
         return
     fi
 
@@ -61,26 +73,49 @@ testOneProgram() {
     echo $CC -g -O0 -Wno-everything -I$CSMITH_PATH -fsanitize=address $NewProgName
     $CC -g -O0 -Wno-everything -I$CSMITH_PATH -fsanitize=address $NewProgName &> /dev/null
     sleep 0.5
-    runCommandCheckString "AddressSanitizer"
-    echo "AS finished"
+    echo "runing UndefinedBehaviorSanitizer"
+    runCommandCheckString "AddressSanitizer" $NewProgName
     res2=$?
+    echo "AS finished"
 
     if [ $res1 -ne $res2 ]; then
         echo "found a bug"
-        echo $programName >> $OUTPUTFILE
+        echo "found a bug in $programName" >> $OUTPUTFILE
     else
         echo "does not find a bug"
     fi
 
 }
 
+
+ProgressBar() {
+    # Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    # Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+    # 1.2 Build progressbar strings and print the ProgressBar line
+    # 1.2.1 Output example:
+    # 1.2.1.1 Progress : [########################################] 100%
+    printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
 testing() {
     echo "======starting=====" >> $OUTPUTFILE
     cd $MutantHome
-    for filename in $MutantHome/*.c; do
-        #echo $filename
+    res=`find $MutantHome -name "*.c"`
+    totalNum=`echo "$res" | wc -l`
+    number=1
+    while read myline
+    do
         testOneProgram $filename
-    done
+        ProgressBar ${number} ${totalNum}
+        ((number++))
+    done <<< "$res"
 }
 
 testing
