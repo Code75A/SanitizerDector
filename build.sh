0921996a0ca29447adca4029de4dbae503f14696
ZY546 <2358413808@qq.com>
2024-11-20 14:17:32 +0800

    DLLVM_DIR of build

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ export CXX=clang++-14
 rm -rf build/
 mkdir build/
 cd build/
-cmake -DLLVM_DIR=/usr/lib/llvm-14/cmake ..
+cmake -DLLVM_DIR=/usr/lib/llvm-14/lib/cmake/llvm -DClang_DIR=/usr/lib/llvm-14/lib/cmake/clang ..
 make
