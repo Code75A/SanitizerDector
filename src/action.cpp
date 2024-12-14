7e41d628dd8a86a419ca20d92776d5cc5cad6e22
Code75 <121840222+Code75A@users.noreply.github.com>
2024-12-14 19:42:12 +0800

    12.14 support 'shf' arg

--- src/action.cpp
+++ src/action.cpp
@@ -94,10 +94,18 @@ namespace sseq
 
     }
 
-    void generateArray(int n,std::string v_name,std::string *insertStr){
+//TODO: getSubExpr -> function
+
+    void generateArray(int n,std::string v_name,std::string *insertStr,const std::string mode){
+        
         std::string num=std::to_string(n);
         //std::string a="SaniTestArr";
-        *insertStr=("\n\tSaniCatcher0=SaniTestArr0["+v_name+"-1];\n");
+        if(mode=="div")
+            *insertStr=("\n\tSaniCatcher0=SaniTestArr0["+v_name+"-1];\n");
+        else if(mode=="shf")
+            *insertStr=("\n\tSaniCatcher=SaniTestArr["+v_name+"];\n");
+        else
+            std::cout<<"Error: generateArray encounter invalid mode type."<<std::endl;
         return ;
     }
 
@@ -117,6 +125,130 @@ namespace sseq
         Cat+=";\n";
 
         return Arr+Cat;
+    }
+    std::string initSani_shf(int bits){
+        if(bits==-1) return "";
+
+        std::string num=std::to_string(bits);
+        std::string Arr="int SaniTestArr["+num+"];\n";
+
+        std::string Cat="int SaniCatcher;\n";
+
+        return Arr+Cat;
+    }
+
+    void SeqASTVisitor::judgeShf(const clang::BinaryOperator *bop,const clang::BinaryOperator* last_bop,std::string* insertStr,clang::SourceManager& SM,int& bits){
+        if(bop == nullptr){
+            std::cout<<"warnning:encounter nullptr,maybe not a binaryoperator\n";
+            return ;
+        }
+
+        //c makes nonsense
+
+        clang::BinaryOperator::Opcode op = bop->getOpcode();
+        clang::Expr *lhs=bop->getLHS();clang::Expr *rhs=bop->getRHS();
+
+        if(Tool::get_stmt_string(lhs).find("+ (MUT_VAR)")!=-1){
+            std::cout<<"check lhs:"<<Tool::get_stmt_string(lhs)<<std::endl;
+            
+            const clang::BinaryOperator *bopl=llvm::dyn_cast<clang::BinaryOperator>(lhs);
+            while(!bopl)
+            {
+                std::string type=lhs->getStmtClassName();
+                std::cout<<"lhs type:"<<type<<std::endl;
+                if(type=="ParenExpr"){
+                    clang::ParenExpr *plhs = llvm::dyn_cast<clang::ParenExpr>(lhs);
+                    lhs=plhs->getSubExpr();
+                }
+                else if(type=="ImplicitCastExpr"){
+                    clang::ImplicitCastExpr *plhs = llvm::dyn_cast<clang::ImplicitCastExpr>(lhs);
+                    lhs=plhs->getSubExpr();
+
+                    std::cout<<Tool::get_stmt_string(lhs)<<std::endl;
+                    std::cout<<"Class:"<<lhs->getStmtClassName()<<std::endl;
+                }
+                else if(type=="UnaryOperator"){
+                    clang::UnaryOperator *plhs = llvm::dyn_cast<clang::UnaryOperator>(lhs);
+                    lhs=plhs->getSubExpr();
+                }
+                else if(type=="CStyleCastExpr"){
+                    clang::CStyleCastExpr *plhs = llvm::dyn_cast<clang::CStyleCastExpr>(lhs);
+                    lhs=plhs->getSubExpr();
+                }
+                else if(type=="CallExpr"){
+                    for (auto &exp : lhs->children() ){;}
+                    std::cout<<"Debug: Unexpected CallExpr."<<std::endl;
+                }
+                bopl=llvm::dyn_cast<clang::BinaryOperator>(lhs);
+            }
+            //std::cout<<"SUCC:"<<Tool::get_stmt_string(lhs)<<std::endl;
+                judgeShf(bopl,bop,insertStr,SM,bits);
+        }
+        else if(Tool::get_stmt_string(rhs).find("+ (MUT_VAR)")!=-1){
+            std::cout<<"check rhs:"<<Tool::get_stmt_string(rhs)<<std::endl;
+
+            const clang::BinaryOperator *bopr=llvm::dyn_cast<clang::BinaryOperator>(rhs);
+            while(!bopr)
+            {
+                std::string type=rhs->getStmtClassName();
+                std::cout<<"rhs type:"<<type<<std::endl;
+                if(type=="ParenExpr"){
+                    clang::ParenExpr *prhs = llvm::dyn_cast<clang::ParenExpr>(rhs);
+                    rhs=prhs->getSubExpr();
+                }
+                else if(type=="ImplicitCastExpr"){
+                    clang::ImplicitCastExpr *prhs = llvm::dyn_cast<clang::ImplicitCastExpr>(rhs);
+                    rhs=prhs->getSubExpr();
+                }
+                else if(type=="UnaryOperator"){
+                    clang::UnaryOperator *prhs = llvm::dyn_cast<clang::UnaryOperator>(rhs);
+                    rhs=prhs->getSubExpr();
+                }
+                else if(type=="CStyleCastExpr"){
+                    clang::CStyleCastExpr *prhs = llvm::dyn_cast<clang::CStyleCastExpr>(rhs);
+                    rhs=prhs->getSubExpr();
+                }
+                else if(type=="CallExpr"){
+                    for (auto &exp : rhs->children() ){
+                            for (auto &exp : rhs->children() ){;}
+                            std::cout<<"Debug: Unexpected CallExpr."<<std::endl;
+                        }
+                }
+
+
+                bopr=llvm::dyn_cast<clang::BinaryOperator>(rhs);
+            }
+            //std::cout<<"SUCC:"<<Tool::get_stmt_string(rhs)<<std::endl;
+                judgeShf(bopr,bop,insertStr,SM,bits);
+        }
+        else{
+            std::cout<<"then rhs==mutvar:"<<Tool::get_stmt_string(rhs)<<std::endl;
+            clang::Expr *fin_lhs=last_bop->getLHS();clang::Expr *fin_rhs=last_bop->getRHS();
+
+            std::string type=fin_lhs->getStmtClassName();
+            while(type=="ParenExpr"){
+                clang::ParenExpr *plhs = llvm::dyn_cast<clang::ParenExpr>(fin_lhs);
+                fin_lhs=plhs->getSubExpr();
+                type=fin_lhs->getStmtClassName();
+            }
+
+
+            if(type=="ImplicitCastExpr"||type=="CStyleCastExpr"){
+                bits=_ctx->getTypeSize(fin_lhs->getType());
+            }
+            else {
+                std::cout<<"type:"<<type<<std::endl;
+                bits=-1;
+                std::cout<<"Debug: Unexpected situation when generate shf_Array."<<std::endl;
+            }
+
+            std::cout<<"Fin_Type:"<<fin_lhs->getType().getAsString()<<std::endl;
+
+            generateArray(1,Tool::get_stmt_string(fin_rhs),insertStr,"shf");
+
+        }
+        
+        return ;
     }
 
     void SeqASTVisitor::judgePrint(const clang::BinaryOperator *bop,clang::SourceManager& SM,const int c,clang::SourceLocation &loc){
@@ -247,9 +379,8 @@ namespace sseq
             std::cout<<"check"<<SM.getSpellingColumnNumber(rhs->getBeginLoc())<<std::endl;
             std::cout<<"check"<<SM.getSpellingColumnNumber(rhs->getEndLoc())<<std::endl;
             count=1;
-            generateArray(count,Tool::get_stmt_string(rhs),insertStr);
+            generateArray(count,Tool::get_stmt_string(rhs),insertStr,"div");
         }
-        //std::cout<<"check"<<SM.getSpellingColumnNumber(lhs->getEndLoc())<<"  ";
         
         std::cout<<"check lhs:"<<Tool::get_stmt_string(lhs)<<std::endl;
         if((Tool::get_stmt_string(lhs).find('/')!=-1 || Tool::get_stmt_string(lhs).find('%')!=-1) && SM.getSpellingColumnNumber(lhs->getEndLoc()) >= c){
@@ -348,7 +479,7 @@ namespace sseq
     void find_UB(int &l,int &c,const std::string& fname){
         std::ifstream file(fname);
         if(!file){
-            std::cout<<"file not exists"<<std::endl;
+            std::cout<<"file not exists--in find_UB"<<std::endl;
             return ;
         }
         l=0;
@@ -363,6 +494,62 @@ namespace sseq
         }
         l=-1;
         return;
+    }
+    void find_INTOP(std::pair<int,int> &intopl, std::pair<int,int> &intopr,const std::string& fname,int UB_line){
+        std::ifstream file(fname);
+        if(!file){
+            std::cout<<"file not exists--in find_INTOP"<<std::endl;
+            return ;
+        }
+        int temp_l=0;
+        std::string cur;
+        std::string def,INTOP_sign,MUT_VAR;
+
+        std::string INTOP_L,INTOP_R;
+        while(std::getline(file,cur)){
+            temp_l++;
+
+            if(temp_l<UB_line+1)
+                continue;
+            else if(temp_l==UB_line+1){
+                std::istringstream iss(cur);
+                iss>>def>>INTOP_sign>>MUT_VAR;
+                INTOP_R=INTOP_L=INTOP_sign;
+
+                // if(INTOP_sign[6]=='L')
+                //     INTOP_R[6]='R';
+                // else if(INTOP_sign[6]=='R')
+                //     INTOP_L[6]='L';
+                // else{
+                //     std::cout<<"Error: Invalid INTOP form."<<std::endl;
+                //     return;
+                // }
+                // continue;
+            }
+            else{
+                // int l=cur.find(INTOP_L);
+                // int r=cur.find(INTOP_R);
+                // if( (l!=-1 && r==-1) || (l==-1 && r!=-1)){
+                //     std::cout<<"Error: INTOP_L/R not in the same row."<<std::endl;
+                //     return ;
+                // }
+                // if(l != -1 &&r != -1){
+                //     intopr.first=intopl.first=temp_l;
+                //     intopl.second=l;
+                //     intopr.second=r;
+                //     return;
+                // }
+
+                int c=cur.find(INTOP_sign+")");
+                if(c!=-1){
+                    intopl.first=temp_l;
+                    intopl.second=c;
+                    return;
+                }
+            }
+        }
+        return ;
+
     }
 
     bool SeqASTVisitor::VisitFunctionDecl(clang::FunctionDecl *func_decl){
@@ -400,6 +587,17 @@ namespace sseq
         else
             std::cout<<"UBFUZZ in line"<<UBFUZZ_line<<std::endl;
         //--------UBFUZZ localized end--------
+
+        std::pair<int,int> intopl_loc=std::make_pair(-1,-1);
+        std::pair<int,int> intopr_loc=std::make_pair(-1,-1);
+        int bits=-1;
+        //--------shift redirection--------        
+        if(!getFlag(MAIN_BIT)){
+            find_INTOP(intopl_loc,intopr_loc,filePath,UBFUZZ_line);
+            UBFUZZ_line=intopl_loc.first;
+        }
+        std::cout<<"check:"<<intopl_loc.first<<":"<<intopl_loc.second<<" & "<<intopr_loc.second<<std::endl;
+        //--------shift redirection end -------- 
 
         std::string func_name = func_decl->getNameAsString();
         std::cout<<"\n| Function [" << func_decl->getNameAsString() << "] defined in: " << filePath << "\n";
@@ -497,9 +695,8 @@ namespace sseq
                         *insertStr="";
                             
                         if(getFlag(MAIN_BIT)){
-
                             if(getFlag(OPT_BIT)){
-                                printf("mode: print\n");
+                                std::cout<<"mode: print"<<std::endl;
 
                                 clang::SourceLocation loc=ori_stmt->getBeginLoc();
                                 judgePrint(bop,SM,UBFUZZ_column,loc);
@@ -511,7 +708,7 @@ namespace sseq
                                     _rewriter.InsertTextBefore(ori_stmt->getBeginLoc(),"fprintf(stderr, \"ACT_CHECK_CODE\");\n");
                             }
                             else{
-                                printf("mode: div\n");
+                                std::cout<<"mode: div"<<std::endl;
 
                                 std::string stmt_string=Tool::get_stmt_string(stmt);
                                 if(stmt_string.find('/')!=-1 || stmt_string.find('%')!=-1)
@@ -520,11 +717,22 @@ namespace sseq
                                 insertStr->pop_back();
                                 *insertStr += "/*A_QUITE_UNIQUE_FLAG*/\n";
                                 _rewriter.InsertTextBefore(ori_stmt->getBeginLoc(),*insertStr);
-                            }
-                            
+                            }   
                         }
                         else{
-                            //TODO
+                            std::cout<<"mode: shift"<<std::endl;
+
+                            std::string stmt_string=Tool::get_stmt_string(stmt);
+
+                            std::cout<<stmt_string<<"-----"<<std::endl;
+
+                            //UBFUZZ_column=stmt_string.find("+ (MUT_VAR)");
+
+                            
+                            judgeShf(bop,nullptr,insertStr,SM,bits);
+
+                            _rewriter.InsertTextBefore(ori_stmt->getBeginLoc(),*insertStr);
+
                         }
 
                     }
@@ -532,7 +740,11 @@ namespace sseq
                     //   std::string lhs_class = lhs->getStmtClassName(),rhs_class = rhs->getStmtClassName();//需要考虑运算符左右子语句可能也是二元运算。所以可能需要设计递归函数
                 }
             }
-            _rewriter.InsertTextBefore(func_decl->getBeginLoc(),initSani(count));
+            if(getFlag(MAIN_BIT))
+                _rewriter.InsertTextBefore(func_decl->getBeginLoc(),initSani(count));
+            else
+                _rewriter.InsertTextBefore(func_decl->getBeginLoc(),initSani_shf(bits));
+                
         }
         return true;
     }
